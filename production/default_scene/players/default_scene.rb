be4aea845b565bb0631fe165deaa337e9d154d62
Eric Meyer <emeyer@8thlight.com>
2009-10-27 17:05:20 -0500

    adding files

---
+++ production/default_scene/players/default_scene.rb
@@ -0,0 +1,5 @@
+module DefaultScene
+  # def scene_opened(event)
+  #   production.calculator.register_display_observer(scene.find("display")) if production.calculator
+  # end
+end
---
+++ production/default_scene/players/display.rb
@@ -0,0 +1,9 @@
+module Display
+  # def casted
+  #   production.calculator.register_display_observer(self)
+  # end
+  # 
+  # def observe(message)
+  #   self.text = message
+  # end
+end
