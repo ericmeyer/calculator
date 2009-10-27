59b3f89e663d441d6bd2a66ef3e15ee4da618cd6
Eric Meyer <emeyer@8thlight.com>
2009-10-27 14:50:32 -0500

    start on calculator class

---
+++ spec/__lib/calculator_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "calculator"
+
+describe Calculator do
+  it "should exist" do
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'limelight/specs/spec_helper'
 # Set the Gem home to the frozen gems
 $PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../production")
 Gem.use_paths(File.join($PRODUCTION_PATH , "__resources", "gems"), Gem.default_path)
-
+$: << File.join($PRODUCTION_PATH, "__lib")
 
 Dir.glob(File.join("__resources", "gems", "gems", "**", "lib")).each do |dir|
   $: << dir
